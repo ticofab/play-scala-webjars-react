3ac6ae0ce6e6f35051c0341ec2b97e6a69354fe9
Fabio Tiriticco <fabio.europe@gmail.com>
2015-06-14 18:12:48 +0200

    initial commit

---
+++ app/assets/javascripts/reactTest.jsx
@@ -0,0 +1,48 @@
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        //{this.props.children}
+        {marked(this.props.children.toString())}
+      </div>
+    );
+  }
+});
+
+React.render(<CommentBox />, document.getElementById('content'));
---
+++ build.sbt
@@ -0,0 +1,22 @@
+name := """play-scala-react"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala, SbtWeb)
+
+scalaVersion := "2.11.6"
+
+libraryDependencies ++= Seq(
+  cache,
+  ws,
+  specs2 % Test,
+  "org.webjars" %% "webjars-play" % "2.4.0-1",
+  "org.webjars" % "react" % "0.13.3",
+  "org.webjars" % "marked" % "0.3.2"
+)
+
+resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+
+// Play provides two styles of routers, one expects its actions to be injected, the
+// other, legacy style, accesses its actions statically.
+routesGenerator := InjectedRoutesGenerator
