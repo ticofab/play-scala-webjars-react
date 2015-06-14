9c04d1ce827912bf081f0955929328f4306176d1
Fabio Tiriticco <fabio.europe@gmail.com>
2015-06-14 18:28:51 +0200

    dynamic data from son

--- app/assets/javascripts/reactTest.jsx
+++ app/assets/javascripts/reactTest.jsx
@@ -1,9 +1,16 @@
+// without the id, we would get a warning like this:
+// Warning: Each child in an array or iterator should have a unique "key" prop. Check the render method of CommentList. See https://fb.me/react-warning-keys for more information.
+var data = [
+  {id: "1234", author: "Pete Hunt", text: "This is one comment"},
+  {id: "5678", author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -12,10 +19,17 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment key={comment.id} author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -33,16 +47,19 @@ var CommentForm = React.createClass({
 
 var Comment = React.createClass({
   render: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        //{this.props.children}
-        {marked(this.props.children.toString())}
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
 });
 
-React.render(<CommentBox />, document.getElementById('content'));
+React.render(
+  <CommentBox data={data} />,
+  document.getElementById('content'));
